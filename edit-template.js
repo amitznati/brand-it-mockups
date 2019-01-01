8cf555ea991b4523bdc60dbe5a6c2a6b5d19daea
AmitZnati <amitznati@gmail.com>
2019-01-01 23:20:29 +0200

    adit-template-full

--- edit-template.js
+++ edit-template.js
@@ -1,5 +1,16 @@
 $('#text-layout-properties').hide(); 
 $('#shape-layout-properties').hide();
+
+  // Make the dashboard widgets sortable Using jquery UI
+  $('.connectedSortable').sortable({
+    placeholder         : 'sort-highlight',
+    connectWith         : '.connectedSortable',
+    handle              : '.box-header, .nav-tabs',
+    forcePlaceholderSize: true,
+    zIndex              : 999999
+  });
+  $('.connectedSortable .box-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move');
+
  // jQuery UI sortable for the todo list
   var sorted = $('.todo-list').sortable({
     placeholder         : 'sort-highlight',
